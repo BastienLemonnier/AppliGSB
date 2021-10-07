b4a050f383b987ee5bddf9eabf79b28cbb644120
BastienLemonnier <lemonnierbastien4@gmail.com>
2021-10-07 17:23:37 +0200

    creation visiteur dao

--- src/gsb/modele/dao/VisiteurDao.java
+++ src/gsb/modele/dao/VisiteurDao.java
@@ -5,6 +5,12 @@
  */
 package gsb.modele.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import gsb.modele.Localite;
+import gsb.modele.Visiteur;
+
 /**
  * @author LEMONNIER Bastien
  * 7 oct. 2021
@@ -12,5 +18,35 @@ package gsb.modele.dao;
  */
 public class VisiteurDao {
 
+	public static Visiteur rechercher(String matricule)
+	{
+		Visiteur unVisiteur = null;
+		String req = "SELECT * FROM Visiteur WHERE MATRICULE = '" + matricule + "' LIMIT 1;";
+		ResultSet resultat = ConnexionMySql.execReqSelection(req);
+		
+		try {
+			if(resultat.next())
+			{
+				String nom = resultat.getString(2);
+				String prenom = resultat.getString(3);
+				String login = resultat.getString(4);
+				String mdp = resultat.getString(5);
+				String adresse = resultat.getString(6);
+				String codePostal = resultat.getString(7);
+				String telephone = resultat.getString(8);
+				Localite laLocalite = LocaliteDao.rechercher(codePostal);
+				String dateEntree = resultat.getString(9);
+				int prime = resultat.getInt(10);
+				String codeUnite = resultat.getString(11);
+				String nomUnite = resultat.getString(12);
+				
+				unVisiteur = new Visiteur(matricule, nom, prenom, login, mdp, adresse, laLocalite, telephone, dateEntree, prime, codeUnite, nomUnite);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return unVisiteur;
+	}
 	
 }
